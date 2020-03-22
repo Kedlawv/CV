7cec5fefa237c127bd8738f6f78fdcc8696de80d
Kedlawv <waldek.bus@gmail.com>
2020-03-22 18:27:16 +0100

    grid layout lines | minor json changes | added js for detecting last property in for in loop | added languages row to html

--- curriculum-vitae/grid-layout-tests/cv.js
+++ curriculum-vitae/grid-layout-tests/cv.js
@@ -22,6 +22,7 @@ function populate(json) {
     const edu = document.getElementById("edu");
     const tech = document.getElementById("tech");
     const soft = document.getElementById("soft");
+    const lan = document.getElementById("lan");
 
 
     populateFromObject(json.social, socialTitle, social);
@@ -29,51 +30,56 @@ function populate(json) {
     populateFromArrayOfObjects(json.experience, exp);
     populateFromArrayOfObjects(json.education, edu);
     populateFromArray(json.technical_skills, tech);
-    populateFromArray(json.soft_skills,soft);
+    populateFromArray(json.soft_skills, soft);
+    populateFromArray(json.languages, lan);
 
 }
 
 function populateFromObject(json, left, right) {
-    const ulLeft = document.createElement("ul");
-    for (const prop in json) {
-        const li = document.createElement("li");
-        li.innerHTML = prop;
-        ulLeft.appendChild(li);
-    }
-    left.appendChild(ulLeft);
 
-    const ulRight = document.createElement("ul");
     for (const prop in json) {
-        const li = document.createElement("li");
-        li.innerHTML = json[prop];
-        ulRight.appendChild(li);
+        const p = document.createElement("p");
+        p.innerHTML = prop;
+        left.appendChild(p);
     }
-    right.appendChild(ulRight);
+
+    for (const prop in json) {
+        const p = document.createElement("p");
+        if (/.+\/+/.test(json[prop])) {
+            let a = document.createElement("a");
+            a.setAttribute("href", json[prop]);
+            a.setAttribute("alt", json[prop]);
+            a.innerHTML = json[prop];
+            p.appendChild(a);
+        } else {
+            p.innerHTML = json[prop];
+        }
+        right.appendChild(p);
+    }
 }
 
 function populateFromArrayOfObjects(json, right) {
 
     for (let i = 0; i < json.length; i++) { //job object
-        const ulRight = document.createElement("ul");
-        let li = undefined;
+
+        let p = document.createElement("p");
         for (const prop in json[i]) {
             if (prop === "start-date") {
-                li = document.createElement("li");
-                li.setAttribute("id", "date");
-                li.innerHTML = json[i][prop];
+                p.innerHTML = json[i][prop];
                 continue;
             } else if (prop === "end-date") {
-                li.innerHTML += " - ";
+                p.innerHTML += " - " + json[i][prop] + "<br>";
+                continue;
             } else if (prop === "company" || prop === "inst-name") {
-                li = document.createElement("li");
-                li.setAttribute("class", "strong");
-            } else {
-                li = document.createElement("li");
+                let strong = document.createElement("strong");
+                strong.innerHTML = json[i][prop] + "<br>";
+                p.appendChild(strong);
+                continue;
             }
-            li.innerHTML += json[i][prop];
-            ulRight.appendChild(li);
+
+            p.innerHTML += json[i][prop] + "<br>";
         }
-        right.appendChild(ulRight);
+        right.appendChild(p);
 
     }
 }
@@ -83,10 +89,15 @@ function populateFromArray(json, right) {
     let ul = undefined;
     let ulDiv = undefined;
     let counter = 3;
+    let i = 0;
+    let length = Object.keys(json).length;
+
     for (const prop in json) {
+
+        console.log("prop: " + prop + "value: " + json[prop]);
         if (counter === 3) {
             ulDiv = document.createElement("div");
-            ulDiv.setAttribute("class","ulDiv");
+            ulDiv.setAttribute("class", "ulDiv");
             ul = document.createElement("ul");
             ul.setAttribute("class", "bulleted");
             ulDiv.appendChild(ul);
@@ -96,7 +107,8 @@ function populateFromArray(json, right) {
         li.innerHTML = json[prop];
         ul.appendChild(li);
         counter--;
-        if (counter === 0) {
+        i++;
+        if (counter === 0 || i === length) {
             right.appendChild(ulDiv);
             counter = 3;
         }
